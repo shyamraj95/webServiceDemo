c1648fe095b757c32628882dc4355dc8d4dfdef8
shyam <shyam@DESKTOP-GTJV0H8>
2021-06-05 20:44:06 +0530

    Second Commit

--- src/main/java/com/webServiceDemo/ws/ui/contoller/UserController.java
+++ src/main/java/com/webServiceDemo/ws/ui/contoller/UserController.java
@@ -1,5 +1,28 @@
 package com.webServiceDemo.ws.ui.contoller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.webServiceDemo.ws.ui.model.request.UserDetailsRequestModel;
+
+@RestController
+@RequestMapping("/users") // http://localhost:8080/users
+//@CrossOrigin(origins= {"http://localhost:8083", "http://localhost:8084"})
+public class UserController {
+	@GetMapping
+	public String getUser() {
+		return "get User was called";
+	}
+	@PostMapping
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		return "create User was called";
+	}
+	@DeleteMapping
+	public String deleteUser() {
+		return "User was deleted";
+	}
 }
